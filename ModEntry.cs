d18b7e5c6bc518e7d5ae9917742b916dfa19d9a4
ferrles124 <mehmetemredursun9@gmail.com>
2026-06-28 22:56:13 +0300

    Implement ModEntry class for AutoNamer mod

---
+++ ModEntry.cs
@@ -0,0 +1,34 @@
+using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley.Menus;
+using System.Reflection;
+
+namespace AutoNamer
+{
+    public class ModEntry : Mod
+    {
+        private static IModHelper? _helper;
+        private static int _counter = 0;
+
+        public override void Entry(IModHelper helper)
+        {
+            _helper = helper;
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+
+            foreach (var ctor in typeof(NamingMenu).GetConstructors(
+                BindingFlags.Public | BindingFlags.Instance))
+            {
+                harmony.Patch(
+                    original: ctor,
+                    postfix: new HarmonyMethod(typeof(ModEntry), nameof(NamingMenu_Postfix))
+                );
+            }
+        }
+
+        private static void NamingMenu_Postfix(NamingMenu __instance)
+        {
+            _counter++;
+            __instance.textBox.Text = $"Isim{_counter}";
+        }
+    }
+}
