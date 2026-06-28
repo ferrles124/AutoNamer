c617ea6a81597d1fd83e0ceeda635d8e8ef54742
ferrles124 <mehmetemredursun9@gmail.com>
2026-06-28 23:28:25 +0300

    Refactor ModEntry to support WaystoneNameMenu

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,34 +1,78 @@
 using HarmonyLib;
 using StardewModdingAPI;
-using StardewValley.Menus;
+using System;
+using System.Linq;
 using System.Reflection;
 
 namespace AutoNamer
 {
     public class ModEntry : Mod
     {
-        private static IModHelper? _helper;
+        private static IMonitor? _monitor;
         private static int _counter = 0;
 
         public override void Entry(IModHelper helper)
         {
-            _helper = helper;
+            _monitor = this.Monitor;
             var harmony = new Harmony("Mehmet.AutoNamer");
 
-            foreach (var ctor in typeof(NamingMenu).GetConstructors(
-                BindingFlags.Public | BindingFlags.Instance))
+            // Waystones DLL'i yüklendiyse, içindeki WaystoneNameMenu tipini bul
+            var waystonesAssembly = AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(a => a.GetName().Name == "StardewWaystones");
+
+            if (waystonesAssembly == null)
+            {
+                _monitor.Log("Stardew Waystones bulunamadı, AutoNamer bu oturumda pasif.", LogLevel.Warn);
+                return;
+            }
+
+            var menuType = waystonesAssembly.GetTypes()
+                .FirstOrDefault(t => t.Name == "WaystoneNameMenu");
+
+            if (menuType == null)
+            {
+                _monitor.Log("WaystoneNameMenu tipi bulunamadı, AutoNamer bu oturumda pasif.", LogLevel.Warn);
+                return;
+            }
+
+            // Tüm public/non-public constructor'ları yakala
+            foreach (var ctor in menuType.GetConstructors(
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
             {
                 harmony.Patch(
                     original: ctor,
-                    postfix: new HarmonyMethod(typeof(ModEntry), nameof(NamingMenu_Postfix))
+                    postfix: new HarmonyMethod(typeof(ModEntry), nameof(WaystoneNameMenu_Postfix))
                 );
             }
+
+            _monitor.Log("AutoNamer, WaystoneNameMenu'ye basariyla baglandi.", LogLevel.Info);
         }
 
-        private static void NamingMenu_Postfix(NamingMenu __instance)
+        private static void WaystoneNameMenu_Postfix(object __instance)
         {
-            _counter++;
-            __instance.textBox.Text = $"Isim{_counter}";
+            try
+            {
+                _counter++;
+                string newName = $"Isim{_counter}";
+
+                var type = __instance.GetType();
+                var field = type.GetField("inputText",
+                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                if (field != null)
+                {
+                    field.SetValue(__instance, newName);
+                    _monitor?.Log($"Otomatik isim atandi: {newName}", LogLevel.Info);
+                }
+                else
+                {
+                    _monitor?.Log("inputText alani bulunamadi.", LogLevel.Warn);
+                }
+            }
+            catch (Exception ex)
+            {
+                _monitor?.Log($"WaystoneNameMenu_Postfix hata: {ex}", LogLevel.Error);
+            }
         }
     }
 }
